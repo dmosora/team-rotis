4add8329801f03877fae3fb6c4178a0ce0ec9213
bkempert <bkempert@dd49fdb1-b540-5232-e5e0-729e3957c749>
2012-02-10 01:39:37 +0000

    bk - fixed some things and added size selection to new windows. have to fix some memory leak though.

---
+++ trunk/TeamRotis/sizeSetWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace SampleCode
+{
+    /// <summary>
+    /// Interaction logic for sizeSetWindow.xaml
+    /// </summary>
+    public partial class sizeSetWindow : Window
+    {
+        MainWindow parentForm;
+        public sizeSetWindow(ref MainWindow form1)
+        {
+            parentForm = form1;
+            InitializeComponent();
+        }
+
+        private void okayBut_Click(object sender, RoutedEventArgs e)
+        {
+            if (widthText.Text == "")
+            {
+                MessageBox.Show("Please enter a width.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            else if (heightText.Text == "")
+            {
+                MessageBox.Show("Please enter a height.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            else
+            {
+                parentForm.result = widthText.Text + "x" + heightText.Text;
+                this.Close();
+            }
+        }
+
+        private void cancelBut_Click(object sender, RoutedEventArgs e)
+        {
+            parentForm.result = "Cancel";
+            this.Close();
+        }
+
+
+    }
+}
